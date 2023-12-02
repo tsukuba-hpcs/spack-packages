609ffc2aef773ca7ff7c0df7d8f2424c246232cf
range3 <white.line.oar@gmail.com>
2023-12-02 22:23:00 +0900

    Change chfs +verbs variant to True by default

--- packages/chfs/package.py
+++ packages/chfs/package.py
@@ -17,7 +17,7 @@ class Chfs(AutotoolsPackage):
     version('1.0.0', sha256='315295bf10b3b3fb93620791e844c540f6f238b4eab6a5c56825c6b7896737a2')
     # version('1.0.0-exp', git='', branch='experimental')
 
-    variant('verbs', default=False, description='enable verbs')
+    variant('verbs', default=True, description='enable verbs')
     variant('pandoc', default=False, description='generate manual pages')
     variant('pmemkv', default=True, description='use pmemkv instead of a POSIX backend')
     variant('devdax', default=False, when='+pmemkv', description='enable devdax support')
