bf33f97805b5f893723417522ebe5b81921771c2
Kohei Sugihara <sugihara@hpcs.cs.tsukuba.ac.jp>
2024-10-25 14:28:06 +0900

    Add an option to enable psql and switch versions

--- packages/gfarm/package.py
+++ packages/gfarm/package.py
@@ -9,12 +9,14 @@ class Gfarm(AutotoolsPackage):
     git = 'https://github.com/oss-tsukuba/gfarm.git'
 
     version('master', branch='master')
-    version('2.8.0', tag='2.8.0', preferred=True)
+    version('2.8.4', tag='2.8.4', preferred=True)
+    version('2.8.0', tag='2.8.0')
     version('2.7.25', tag='2.7.25')
     
     variant('infiniband', default=False, description='enable InfiniBand')
+    variant('postgresql', default=True, description='enable PostgreSQL')
 
-    depends_on('postgresql')
+    depends_on('postgresql', when="+postgresql")
     depends_on('openssl')
     depends_on('rdma-core', when="+infiniband")
     depends_on('py-docopt')
@@ -27,8 +29,11 @@ class Gfarm(AutotoolsPackage):
 
     def configure_args(self):
         args = []
-
         if '+infiniband' in self.spec:
             args.extend(['--with-infiniband'])
-
         return args
+
+    def setup_run_environment(self, env):
+        env.prepend_path("CPATH", prefix.include)
+        env.prepend_path("LIBRARY_PATH", prefix.lib)
+        env.prepend_path("LD_LIBRARY_PATH", prefix.lib)
